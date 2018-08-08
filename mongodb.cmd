4e9cdb2f6eff972e0258a02133b4971d4c6f5d56
michele <tiboburn@gmail.com>
2018-08-08 11:29:49 +0200

    Implemented CMD script.

---
+++ mongodb.cmd
@@ -0,0 +1,2 @@
+docker-compose -f docker-compose_db-test.yml -f docker-compose_mongodb.yml up --build --abort-on-container-exit --scale worker=2
+docker-compose -f docker-compose_db-test.yml -f docker-compose_mongodb.yml down -v
