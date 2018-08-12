e9cc7cc358f5dec274836676cf4161618c128106
michele <tiboburn@gmail.com>
2018-08-12 08:56:42 +0200

    Changed workers count.

--- cassandra.sh
+++ cassandra.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml -f docker-compose_cassandra.yml up --build --abort-on-container-exit --scale worker=8
+docker-compose -f docker-compose.yml -f docker-compose_cassandra.yml up --build --abort-on-container-exit --scale worker=12
 docker-compose -f docker-compose.yml -f docker-compose_cassandra.yml down -v
