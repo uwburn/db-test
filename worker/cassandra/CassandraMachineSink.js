0527977ea851ff62bb606f59eab5122979abec53
michele <tiboburn@gmail.com>
2018-08-26 11:04:19 +0200

    WIP on Cassandra reads.

--- worker/cassandra/CassandraMachineSink.js
+++ worker/cassandra/CassandraMachineSink.js
@@ -4,9 +4,38 @@ const { Writable } = require('stream');
 
 const cassandra = require('cassandra-driver');
 const FlattenJS = require('flattenjs');
+const _ = require('lodash');
 
 const HIGH_WATERMARK = 256;
 const TIME_COMPLEX_MODE = process.env.CASSANDRA_TIME_COMPLEX_MODE || "BOTH";
+
+const maxTime = 8640000000000000;
+const maxDate = new Date(maxTime);
+
+function subtractValues(o1, o2) {
+  let d = { };
+
+  let f1 = FlattenJS.convert(o1);
+  let f2 = FlattenJS.convert(o2);
+
+  for (let k in f2) {
+    if (isNaN(f2[k]))
+      continue;
+
+    if (isNaN(f1[k])) {
+      d[k] = f2[k];
+      continue;
+    }
+
+    d[k] = f2[k] - f1[k];
+  }
+
+  let res = {};
+  for (let k in d)
+    _.set(res, k, d[k]);
+
+  return res;
+}
 
 module.exports = class CassandraMachineSink {
 
@@ -82,23 +111,267 @@ module.exports = class CassandraMachineSink {
   }
 
   async queryIntervalRange(name, options) {
+    options.select = options.select || {};
 
+    if (options.groups.length === 1)
+      return await this.queryIntervalRangeSingleGroup(name, options);
+    else
+      return await this.queryIntervalRangeMultipleGroups(name, options);
+  }
+
+  async queryIntervalRangeSingleGroup(name, options) {
+    let group = options.groups[0];
+
+    let count = 0;
+    return await new Promise((resolve, reject) => {
+      this.cassandraClient.stream("SELECT * FROM interval WHERE device_type = ? AND device = ? AND group = ? AND (start_time, end_time) >= (?, ?) AND (start_time, end_time) <= (?, ?)", [
+        options.deviceType,
+        options.device,
+        group,
+        options.startTime,
+        options.startTime,
+        options.endTime,
+        maxDate,
+      ], {
+        prepare: true
+      }).on('data', function (row) {
+        ++count;
+      }).on('end', function () {
+        resolve(count);
+      }).on('error', function (err) {
+        reject(err);
+      });
+    });
+  }
+
+  async queryIntervalRangeMultipleGroups(name, options) {
+    throw new Error("Currently not supported, as no query requires it");
   }
 
   async queryTimeComplexRange(name, options) {
+    options.select = options.select || {};
 
+    if (options.groups.length === 1)
+      return await this.queryTimeComplexRangeSingleGroup(name, options);
+    else
+      return await this.queryTimeComplexRangeMultiGroups(name, options);
+  }
+
+  async queryTimeComplexRangeSingleGroup(name, options) {
+    let group = options.groups[0];
+    let paths = options.select[group];
+
+    let count = 0;
+    return await new Promise((resolve, reject) => {
+      this.cassandraClient.stream("SELECT * FROM time_complex WHERE device_type = ? AND device = ? AND group = ? AND timestamp >= ? AND timestamp <= ?", [
+        options.deviceType,
+        options.device,
+        group,
+        options.startTime,
+        options.endTime,
+      ], {
+        prepare: true
+      }).on('data', function (row) {
+        ++count;
+
+        row.value = _.pick(row.value, paths);
+      }).on('end', function () {
+        resolve(count);
+      }).on('error', function (err) {
+        reject(err);
+      });
+    });
+  }
+
+  async queryTimeComplexRangeMultiGroups(name, options) {
+    throw new Error("Currently not supported, as no query requires it");
   }
 
   async queryTimeComplexRangeBucketAvg(name, options) {
+    options.select = options.select || {};
 
+    if (options.groups.length === 1)
+      return await this.queryTimeComplexRangeBucketAvgSingleGroup(name, options);
+    else
+      return await this.queryTimeComplexRangeBucketAvgMultiGroups(name, options);
+  }
+
+  async queryTimeComplexRangeBucketAvgSingleGroup(name, options) {
+    let group = options.groups[0];
+    let paths = options.select[group];
+
+    let duration = options.endTime.getTime() - options.startTime.getTime();
+    let bucketStep = Math.round(duration / options.buckets);
+
+    let buckets = [];
+    for (let i = 0; i < options.buckets; ++i) {
+      buckets[i] = {
+        time: options.startTime.getTime() + i * bucketStep,
+        count: 0,
+        minTime: Number.MAX_SAFE_INTEGER,
+        maxTime: Number.MIN_SAFE_INTEGER
+      };
+
+      for (let path of paths)
+        buckets[i][group + "_" + path] = 0;
+    }
+
+    let count = 0;
+    await new Promise((resolve, reject) => {
+      this.cassandraClient.stream("SELECT * FROM time_complex WHERE device_type = ? AND device = ? AND group = ? AND timestamp >= ? AND timestamp <= ?", [
+        options.deviceType,
+        options.device,
+        group,
+        options.startTime,
+        options.endTime,
+      ], {
+        prepare: true
+      }).on('data', function (row) {
+        ++count;
+
+
+        let bucketIndex = Math.floor((row.timestamp - options.startTime.getTime()) / bucketStep) - 1;
+        let bucket = buckets[bucketIndex];
+
+        if (!bucket)
+          return;
+
+        bucket.minTime = Math.min(bucket.minTime, row.time);
+        bucket.maxTime = Math.max(bucket.maxTime, row.time);
+        ++bucket.count;
+        for (let path of paths)
+          bucket[group + "_" + path + "_avg"] += _.get(row.value, path);
+      }).on('end', function () {
+        resolve(count);
+      }).on('error', function (err) {
+        reject(err);
+      });
+    });
+
+    buckets.filter((b) => b.count > 0).map((b) => {
+      let res = {
+        deviceType: options.deviceType,
+        device: options.device,
+        group: group,
+        time: new Date(b.time),
+        count: b.count,
+      };
+
+      for (let k in b) {
+        if (!k.endsWith("_avg"))
+          continue;
+
+        res[k.substring(0, k.length - 4)] = b[k] / b.count;
+      }
+    });
+
+    return buckets.length;
+  }
+
+  async queryTimeComplexRangeBucketAvgMultiGroups(name, options) {
+    throw new Error("Currently not supported, as no query requires it");
   }
 
   async queryTimeComplexDifference(name, options) {
+    options.select = options.select || {};
 
+    if (options.groups.length === 1)
+      return await this.queryTimeComplexDifferenceSingleGroup(name, options);
+    else
+      return await this.queryTimeComplexDifferenceMultiGroups(name, options);
+  }
+
+  async queryTimeComplexDifferenceSingleGroup(name, options) {
+    let group = options.groups[0];
+    let paths = options.select[group];
+
+    let promises = [];
+    for (let i = 0; i < options.times.length -1; ++i) {
+      let startTime = options.times[i];
+      let endTime = options.times[i + 1];
+
+      promises.push(this.cassandraClient.stream("SELECT * FROM time_complex WHERE device_type = ? AND device = ? AND group = ? AND timestamp >= ? AND timestamp <= ? ORDER BY timestamp ASC LIMIT 1", [
+        options.deviceType,
+        options.device,
+        group,
+        startTime,
+        endTime,
+      ], {
+        prepare: true
+      }));
+
+      promises.push(this.cassandraClient.stream("SELECT * FROM time_complex WHERE device_type = ? AND device = ? AND group = ? AND timestamp >= ? AND timestamp <= ? ORDER BY timestamp DESC LIMIT 1", [
+        options.deviceType,
+        options.device,
+        group,
+        startTime,
+        endTime,
+      ], {
+        prepare: true
+      }));
+    }
+
+    let results = await Promise.all(promises);
+
+    let subs = [];
+    for (let i = 0; i < results.length -1; ++i) {
+      let first = results[i];
+      let last = results[i + 1];
+
+      let sub = {
+        deviceType: first.deviceType,
+        device: first.device,
+        group: first.group,
+        startTime: first.timestamp,
+        endTime: last.timestamp
+      };
+
+      subs.push(_.assign(sub, subtractValues(first, last)));
+    }
+
+    return subs.length;
+  }
+
+  async queryTimeComplexDifferenceMultiGroups(name, options) {
+    throw new Error("Currently not supported, as no query requires it");
   }
 
   async queryTimeComplexLastBefore(name, options) {
+    options.select = options.select || {};
 
+    if (options.groups.length === 1)
+      return await this.queryTimeComplexLastBeforeSingleGroup(name, options);
+    else
+      return await this.queryTimeComplexLastBeforeMultipleGroups(name, options);
+  }
+
+  async queryTimeComplexLastBeforeSingleGroup(name, options) {
+    let group = options.groups[0];
+    let paths = options.select[group];
+
+    let count = 0;
+    return await new Promise((resolve, reject) => {
+      this.cassandraClient.stream("SELECT * FROM time_complex WHERE device_type = ? AND device = ? AND group = ? AND timestamp <= ? ORDER BY timestamp DESC LIMIT 1", [
+        options.deviceType,
+        options.device,
+        group,
+        options.time,
+      ], {
+        prepare: true
+      }).on('data', function (row) {
+        ++count;
+
+        row.value = _.pick(row.value, paths);
+      }).on('end', function () {
+        resolve(count);
+      }).on('error', function (err) {
+        reject(err);
+      });
+    });
+  }
+
+  async queryTimeComplexLastBeforeMultipleGroups(name, options) {
+    throw new Error("Currently not supported, as no query requires it");
   }
 
   async queryTimeComplexTopDifference(name, options) {
