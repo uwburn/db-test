e5835aba225551fdfd301e99592e62cc25b64a39
michele <tiboburn@gmail.com>
2018-08-29 23:07:49 +0200

    Changed parameters.

--- mongodb.sh
+++ mongodb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml -f docker-compose_mongodb.yml up --build --abort-on-container-exit --scale worker=12
+docker-compose -f docker-compose.yml -f docker-compose_mongodb.yml up --build --abort-on-container-exit --scale worker=16
 docker-compose -f docker-compose.yml -f docker-compose_mongodb.yml down -v
