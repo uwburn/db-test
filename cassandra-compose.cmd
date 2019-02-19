eb02eec5d4edccda54d320d150e77e4833de3bcf
Michele Tibaldi <michele.tibaldi@holonix.it>
2019-02-19 07:20:32 +0100

    Restructure compose and scripts for better Swarm support

---
+++ cassandra-compose.cmd
@@ -0,0 +1,5 @@
+docker-compose -f docker-compose_registry.yml up
+docker-compose -f docker-compose_build.yml --build push
+
+docker-compose -f docker-compose_mqtt.yml -f docker-compose_cassandra-seed.yml -f docker-compose_coordinator.yml -f docker-compose_worker.yml up --abort-on-container-exit --scale worker=8
+docker-compose -f docker-compose_mqtt.yml -f docker-compose_cassandra-seed.yml -f docker-compose_coordinator.yml -f docker-compose_worker.yml down -v
--- cassandra.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker-compose -f docker-compose.yml -f docker-compose_cassandra.yml up --build --abort-on-container-exit --scale worker=8
-docker-compose -f docker-compose.yml -f docker-compose_cassandra.yml down -v
