ce8028840983d45d660671b13bbfc995a1a4f777
Michele Tibaldi <tiboburn@gmail.com>
2019-02-19 22:13:25 +0000

    Improve Swarm scripts and compose files

--- swarm/cassandra-cluster.sh
+++ swarm/cassandra-cluster.sh
@@ -5,8 +5,8 @@ docker stack deploy --compose-file docker-compose_cassandra-seed.yml db-test
 
 # Start cassandra node 1
 sleep 60
-docker stack deploy --compose-file docker-compose_cassandra-node.yml db-test
+docker stack deploy --compose-file docker-compose_cassandra-node1.yml db-test
 
 # Start cassandra node 2
 sleep 60
-docker service scale db-test_cassandra-node=2
+docker stack deploy --compose-file docker-compose_cassandra-node2.yml db-test
