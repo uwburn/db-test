0a2e462133696d86703c75e7fd648bf9a3f363cb
Michele Tibaldi <tiboburn@gmail.com>
2019-02-27 22:07:17 +0100

    Add GSI index creation

--- coordinator/buildSuite.js
+++ coordinator/buildSuite.js
@@ -224,5 +224,10 @@ async function prepareMachineDataCouchbaseA(databaseOpts) {
 
   let q = couchbase.N1qlQuery.fromString('CREATE PRIMARY INDEX `db-test_primary` ON `db-test`;');
   await couchbaseBucket.queryAsync(q);
-  
+
+  q = couchbase.N1qlQuery.fromString("CREATE INDEX `db-test_interval_device_times` ON `db-test`(`type`, `device`, `startTime`, `endTime`) WHERE type = 'interval' USING GSI;");
+  await couchbaseBucket.queryAsync(q);
+
+  q = couchbase.N1qlQuery.fromString("CREATE INDEX `db-test_time_complex_device_time` ON `db-test`(`type`, `device`, `time`) WHERE type = 'time_complex' USING GSI;");
+  await couchbaseBucket.queryAsync(q);
 }
