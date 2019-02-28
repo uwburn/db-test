2c65391829d7570af9673231293bd6b6321f5f00
Michele Tibaldi <tiboburn@gmail.com>
2019-02-28 22:05:23 +0100

    Splitted dev configs

--- dev/couchbase_pre.sh
+++ dev/couchbase_pre.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker-compose -f docker-compose.yml -f docker-compose_couchbase.yml up
-#docker-compose -f docker-compose.yml -f docker-compose_couchbase.yml down -v
+docker-compose -f docker-compose.yml -f docker-compose_couchbase.yml down -v
