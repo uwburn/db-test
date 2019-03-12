a1b7a64eb2af417ce2f927c2752841c056e30a18
Michele Tibaldi <tiboburn@gmail.com>
2019-03-12 17:42:04 +0000

    Setup for nine nodes tests

--- swarm/cassandra-cluster.sh
+++ swarm/cassandra-cluster.sh
@@ -4,22 +4,33 @@
 docker stack deploy --compose-file docker-compose_cassandra-seed.yml db-test
 
 # Start cassandra node 1
-sleep 60
+sleep 45
 docker stack deploy --compose-file docker-compose_cassandra-node1.yml db-test
 
 # Start cassandra node 2
-sleep 60
+sleep 45
 docker stack deploy --compose-file docker-compose_cassandra-node2.yml db-test
 
 # Start cassandra node 3
-sleep 60
+sleep 45
 docker stack deploy --compose-file docker-compose_cassandra-node3.yml db-test
 
 # Start cassandra node 4
-sleep 60
+sleep 45
 docker stack deploy --compose-file docker-compose_cassandra-node4.yml db-test
 
 # Start cassandra node 5
-sleep 60
+sleep 45
 docker stack deploy --compose-file docker-compose_cassandra-node5.yml db-test
 
+# Start cassandra node 6
+sleep 45
+docker stack deploy --compose-file docker-compose_cassandra-node6.yml db-test
+
+# Start cassandra node 7
+sleep 45
+docker stack deploy --compose-file docker-compose_cassandra-node7.yml db-test
+
+# Start cassandra node 8
+sleep 45
+docker stack deploy --compose-file docker-compose_cassandra-node8.yml db-test
--- swarm/mongodb-replica.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Start mongodb instances
-docker stack deploy --compose-file docker-compose_mongodb-replica.yml db-test
